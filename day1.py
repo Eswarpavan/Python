9cd88d671788d854baf1c2d3676149e82d47c024
Eswarpavan <61042232+Eswarpavan@users.noreply.github.com>
2022-02-04 02:08:50 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,99 @@
+# print("Hello")
+# print("print hi ")
+
+""" #print("Welcome") This is a comment """
+""" I want to print my name"""
+# print("Eswar Pavan Maganti")
+# print("Line1")
+# print("Line2")
+"""using end"""
+# print("Line1",end=" ")
+# print("Line2")
+""" datatypes in python
+int-->0,1,2,3...
+
+float-> Decimal,3.4,5.6...
+
+string->stream of letters(anything that is written in double coates are string)
+
+bool-> True/False
+
+"""
+
+# print(type(1111))
+# print(type(1.1))
+# print(type("HI"))
+# print(type(True))
+
+""" variables """
+
+# var="HI"
+# print(var," Eswar")
+
+""" take input from keyboard"""
+
+# a=input()
+# print(a)
+
+# a=input()
+# print(a,type(a))
+
+# a=input()
+# b=int(a)
+# print(a,type(a))
+# print(b,type(b))
+
+""" OPERATORS 
++-> ADD
+- -> SUB
+* -> MUL
+/ ->FLOAT DIVISION
+// ->INT DIVISION
+% -> REMAINDER
+= -> VALUE ASSIGNMENT
+== -> COMPARE
+> greater than --bool
+<less than --bool
+** -> power (a to the power b= a**b)
+"""
+# a=5
+# b=3
+# print("add",a+b)
+# print("sub",a-b)
+# print("mul",a*b)
+# print("fd",a/b)
+# print("id",a//b)
+# print("r",a%b)
+
+""" conditional statements
+if
+if -else
+elif
+
+"""
+# a=20
+# if(a>10):
+#     print("given number is greater than 10")
+# a=20
+# if(a>20):
+#     print("given number is greater than 20")
+# else:
+#     print("given number is less than or equal to 20")
+
+# a=20
+# if(a>20):
+#     print("given number is greater than 20")
+# elif (a>10):
+#     print("given number is greater than 10")
+# else:
+#     print("given number is less than or equal to 10")
+
+""" nested if conditions """
+# a=20
+# if(a>10):
+#     if(a>15):
+#         print("given number is greater than 15")
+#     else:
+#         print("given number is greater than ten and less than or equal to 15")
+# else:
+#     print("given number is less than or equal to 10")
---
+++ day2.py
@@ -0,0 +1,55 @@
+""" LOOPS """
+""" Logic:-repeatable statements """
+""" for and while..."""
+"""FOR LOOP"""
+"""necessary keywords for FOR LOOP are
+for 
+in
+"""
+# for i in "INDIA":
+#     print(i)
+# for i in "INDIA":
+#     print(i,end="")
+"""
+range we can use it in 3 styeles
+
+range(10)
+range(0,10)
+range(0,10,1)
+
+"""
+# for i in range(10):
+#     print(i)
+# for i in range(1,10):
+#     print(i)
+# for i in range(0,10,2):
+#     print(i)
+
+""" remeber about indentation and also indexing starts from 0 and ends at n-1"""
+# for i in range(0,-18):
+#     print(i) 
+""" we cannot print negative numbers"""
+# for i in range(-18,-10):
+#     print(i)
+"""range should have - to - to print negative"""
+""" while loop """
+
+# sum=0
+# i=0
+# while(i<5):
+#     sum+=i
+#     i+=1
+# print(sum) #01+2+3+4=10
+"""
+break-immediate termination of loop
+continue - terminates current iteration
+
+these r called loop control statments"""
+# for i in range(10):
+#     if(i==5):
+#         break
+#     print(i)
+# for i in range(10):
+#     if(i==5):
+#         continue
+#     print(i)
