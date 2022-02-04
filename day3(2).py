285c2e4e4e94fc2419607edb753d52211dac5cd2
unknown <eswarpavan50@gmail.com>
2022-02-04 12:02:02 +0530

    Lists

---
+++ day3(2).py
@@ -0,0 +1,7 @@
+"""Lists"""
+
+#list declaration
+#ls=["hi","hello","welcome"]
+#list indexing starts from 0
+ls=["hi","hello","welcome"]
+print(ls[1])
---
+++ list.py
@@ -0,0 +1,34 @@
+"""Lists"""
+
+#list declaration
+#ls=["hi","hello","welcome"]
+#list indexing starts from 0
+# ls=["hi","hello","welcome"]
+# print(ls[1])
+
+# ls=["hi","hello","welcome"]
+#       0    1      2        
+#      -3     -2        -1
+
+"""
+
+ls.append(element)
+ls.insert(index,element)
+del(ls(index))
+slicing --> ls[x:y] x,y are indexes
+ls[x:]
+ls[:y]
+ls[:] -->prints complete list
+max(ls)
+min(ls)
+len(ls)
+
+"""
+
+ls=[1,2,3,4,5]
+# print(sum(ls))
+# print(max(ls))
+# print(min(ls))
+for i in ls:
+    print(ls)
+
